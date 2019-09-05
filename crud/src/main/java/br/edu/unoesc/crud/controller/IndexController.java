5fc2433473a5762abcd0c70797e7a1b15d568f61
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2019-09-05 09:28:25 -0300

    models

--- crud/src/main/java/br/edu/unoesc/crud/controller/IndexController.java
+++ crud/src/main/java/br/edu/unoesc/crud/controller/IndexController.java
@@ -1,4 +1,4 @@
-package br.edu.unoesc.controller;
+package br.edu.unoesc.crud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
