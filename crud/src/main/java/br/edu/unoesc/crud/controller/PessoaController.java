0bf042d9d26a3b9d44fb49920a65dc741fbd29de
LucianoVirmes <luciano.virmes18@gmail.com>
2019-09-11 19:33:52 -0300

    adicionado crud service

--- crud/src/main/java/br/edu/unoesc/crud/controller/PessoaController.java
+++ crud/src/main/java/br/edu/unoesc/crud/controller/PessoaController.java
@@ -2,11 +2,22 @@ package br.edu.unoesc.crud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.edu.unoesc.crud.model.Pessoa;
 
 @Controller
 public class PessoaController {
 
-    @GetMapping("/pessoa/cadastro") public String cadastro(){
+    @GetMapping("/pessoa/cadastro")
+    public String cadastro(){
         return "pessoa/cadastro";
     }
+   
+    
+    @PostMapping("/pessoa/cadastro")
+    public String cadastro(Pessoa pessoa) {
+    	return "pessoa/cadastro";
+    }
+    
 }
---
+++ crud/src/main/java/br/edu/unoesc/crud/service/PessoaService.java
@@ -0,0 +1,33 @@
+package br.edu.unoesc.crud.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.unoesc.crud.model.Pessoa;
+
+@Service
+public class PessoaService implements CrudService<Pessoa> {
+
+	@Override
+	public Pessoa salvar(Pessoa dado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pessoa excluir(Pessoa dado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pessoa> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+}
