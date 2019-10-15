d68643cb98dde664527e1b2cbb2519eca9be750e
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2019-10-15 12:04:27 -0300

    botao logout

--- crud/src/main/java/br/edu/unoesc/crud/controller/LoginController.java
+++ crud/src/main/java/br/edu/unoesc/crud/controller/LoginController.java
@@ -1,5 +1,6 @@
 package br.edu.unoesc.crud.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +12,10 @@ public class LoginController {
         return "login";
     }
 
-
+    @RequestMapping(path = "/logout")
+    public String logout() {
+        SecurityContextHolder.clearContext();
+        return "login";
+    }
 }
 
